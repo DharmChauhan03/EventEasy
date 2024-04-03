a16b0fcf117236f3891cbf7f39a46a36ed249a0a
Dharm Chauhan <108711448+DharmChauhan03@users.noreply.github.com>
2024-04-03 18:06:14 +0530

    Add files via upload

---
+++ EventEasy/js/data.js
@@ -0,0 +1,20 @@
+let movies = [
+    {
+        image: 'img/banner/banner1.png'
+    },
+    {
+        image: 'img/banner/banner2.png'
+    },
+    {
+        image: 'img/banner/banner3.png'
+    },
+    {
+        image: 'img/banner/banner4.png'
+    },
+    {
+        image: 'img/banner/banner5.png'
+    },
+    {
+        image: 'img/banner/banner6.png'
+    }
+]
